472328d881a424d14139494c09faad1093c1ad1b
Trevor Dixon <trevordixon@gmail.com>
2012-09-05 12:56:29 -0600

    Skip comments in ppm.

---
+++ ImageEditor/src/Pixel.java
@@ -0,0 +1,73 @@
+public class Pixel {
+	public int red;
+	public int green;
+	public int blue;
+	
+	public Pixel(int r, int g, int b) {
+		red = r;
+		green = g;
+		blue = b;
+	}
+
+	public Pixel(int v) {
+		red = v;
+		green = v;
+		blue = v;
+	}
+	
+	public Pixel invert() {
+		return invert(255);
+	}
+	
+	public Pixel invert(int max) {
+		return new Pixel(max-red, max-green, max-blue);
+	}
+	
+	public Pixel grayscale() {
+		int v = (red + green + blue) / 3;
+		return new Pixel(v);
+	}
+	
+	public Pixel emboss(Pixel p) {
+		int v = 128 + absMax(
+			red - p.red,
+			blue - p.blue,
+			green - p.green
+		);
+		
+		if (v > 255) v = 255;
+		else if (v < 0) v = 0;
+		
+		return new Pixel(v);
+	}
+	
+	public String toString() {
+		return red + " " + green + " " + blue + " ";
+	}
+	
+	public static int absMax(int...vals) {
+		int max = 0;
+		
+		for (int v : vals) {
+			if (Math.abs(v) > Math.abs(max)) {
+				max = v;
+			}
+		}
+		
+		return max;
+	}
+	
+	public static Pixel average(Pixel...pixels) {
+		int r = 0,
+			g = 0,
+			b = 0;
+		
+		for (Pixel p : pixels) {
+			r += p.red / pixels.length;
+			g += p.green / pixels.length;
+			b += p.blue / pixels.length;
+		}
+		
+		return new Pixel(r, g, b);
+	}
+}
--- ImageEditor/src/PixelMap.java
+++ ImageEditor/src/PixelMap.java
@@ -3,80 +3,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PushbackReader;
 
-class Pixel {
-	public int red;
-	public int green;
-	public int blue;
-	
-	public Pixel(int r, int g, int b) {
-		red = r;
-		green = g;
-		blue = b;
-	}
-
-	public Pixel(int v) {
-		red = v;
-		green = v;
-		blue = v;
-	}
-	
-	public Pixel invert() {
-		return invert(255);
-	}
-	
-	public Pixel invert(int max) {
-		return new Pixel(max-red, max-green, max-blue);
-	}
-	
-	public Pixel grayscale() {
-		int v = (red + green + blue) / 3;
-		return new Pixel(v);
-	}
-	
-	public Pixel emboss(Pixel p) {
-		int v = 128 + absMax(
-			red - p.red,
-			blue - p.blue,
-			green - p.green
-		);
-		
-		if (v > 255) v = 255;
-		else if (v < 0) v = 0;
-		
-		return new Pixel(v);
-	}
-	
-	public String toString() {
-		return red + " " + green + " " + blue + " ";
-	}
-	
-	public static int absMax(int...vals) {
-		int max = 0;
-		
-		for (int v : vals) {
-			if (Math.abs(v) > Math.abs(max)) {
-				max = v;
-			}
-		}
-		
-		return max;
-	}
-	
-	public static Pixel average(Pixel...pixels) {
-		int r = 0,
-			g = 0,
-			b = 0;
-		
-		for (Pixel p : pixels) {
-			r += p.red / pixels.length;
-			g += p.green / pixels.length;
-			b += p.blue / pixels.length;
-		}
-		
-		return new Pixel(r, g, b);
-	}
-}
-
 public class PixelMap {
 	public int width;
 	public int height;
@@ -185,8 +111,15 @@ public class PixelMap {
 	}
 	
 	private PixelMap skipSeparator() throws IOException {
+		
 		int c;
-		while (!isChar(c = stream.read()));
+		while (!isChar(c = stream.read())) {
+			if (c == '#') {
+				while ((char) (c = stream.read()) != '\n' && c > -1);
+				stream.unread(c);
+			}
+		}
+		
 		stream.unread(c);
 		return this;
 	}
