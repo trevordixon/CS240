ca34a741b62845b38d7b714fb9816a3ab102b444
Trevor Dixon <trevordixon@gmail.com>
2012-09-12 09:06:12 -0600

    Starting SpellingCorrector project

---
+++ SpellingCorrector/src/Words.java
@@ -0,0 +1,44 @@
+import java.util.Iterator;
+
+
+public class Words implements Trie, Iterable<Trie.Node> {
+
+	@Override
+	public void add(String word) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Node find(String word) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getWordCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNodeCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Iterator<Trie.Node> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
