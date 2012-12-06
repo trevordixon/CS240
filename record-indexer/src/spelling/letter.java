d9ffff773c1960d31226c7f5fd689d3771bf1e10
Trevor Dixon <trevordixon@gmail.com>
2012-12-06 16:56:22 -0700

    Deal with dashes in spell checking

--- record-indexer/src/spelling/letter.java
+++ record-indexer/src/spelling/letter.java
@@ -2,12 +2,14 @@ package spelling;
 
 public enum letter {
 	A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,
-	SPACE(" ");
+	SPACE(" "),
+	DASH("-");
 	
 	private final String val;
 	
 	public static letter valueOf(char c) {
 		if (c == ' ') return letter.SPACE;
+		if (c == '-') return letter.DASH;
 		
 		try {
 			return letter.valueOf(Character.toString(Character.toUpperCase(c)));
